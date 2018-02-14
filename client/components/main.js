3fbe79bb009611ca8dd52029cec95fd34ce8ae64
Sam Glass <samuelglass@gmail.com>
2018-02-14 17:42:15 -0500

    login and logout render where and when they're supposed to but god are they ugly

--- client/components/main.js
+++ client/components/main.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import { Switch, Route, Link} from 'react-router-dom'
-import {logout, me} from '../store'
-import CodeEditor from './CodeEditor'
-import UserHome from './user-home'
-import {Login, Signup} from './auth-form'
-import {Router} from 'react-router'
-import history from '../history'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Switch, Route, Link } from "react-router-dom";
+import { logout, me } from "../store";
+import CodeEditor from "./CodeEditor";
+import UserHome from "./user-home";
+import { Login, Signup } from "./auth-form";
+import { Router } from "react-router";
+import history from "../history";
 
 /**
  * COMPONENT
@@ -20,118 +20,81 @@ import history from '../history'
 
 class Main extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      test: 'case'
-    }
+      test: "case"
+    };
   }
 
-  componentDidMount () {
-    console.log("component did mount from main fired")
-    this.props.loadInitialData()
+  componentDidMount() {
+    console.log("component did mount from main fired");
+    this.props.loadInitialData();
   }
 
   render() {
-    const {isLoggedIn, handleClick} = this.props
-    console.log("!!!", this.props)
-  return (
-    <div>
-    <a href="#" onClick={handleClick}>Logout</a>
-
-    <Router history={history}>
-    <div>
-    <Link to="/login">Login</Link>
-    <Link to="/signup">Sign Up</Link>
-    <Switch>
-    <Route path="/login" component={Login} />
-    <Route path="/signup" component={Signup} />
-      {
-        isLoggedIn &&
-
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/editor" component={CodeEditor} />
-            <Route path="/home" component={UserHome} />
-            <Route path="/" component={UserHome} />
-          </Switch>
-      }
-      <Route component={Login} />
-
-    </Switch>
-    </div>
-</Router>
-
-
-    </div>
-
-    //   <nav>
-    //     {
-    //       isLoggedIn
-    //         ? <div>
-    //           {/* The navbar will show these links after you log in */}
-    //           <Link to="/home">Home</Link>
-    //           <a href="#" onClick={handleClick}>Logout</a>
-    //         </div>
-    //         : <div>
-    //           {/* The navbar will show these links before you log in */}
-    //           <Link to="/login">Login</Link>
-    //           <Link to="/signup">Sign Up</Link>
-    //         </div>
-    //     }
-    //   </nav>
-    //   <hr />
-
-    //   {children}
-    // </div>
-
-  //   <Router history={history}>
-  //     <Switch>
-  //       {/* Routes placed here are available to all visitors */}
-  //       <Route path="/login" component={Login} />
-  //       <Route path="/signup" component={Signup} />
-  //       {
-  //         isLoggedIn &&
-  //           <Switch>
-  //             {/* Routes placed here are only available after logging in */}
-  //             <Route path="/home" component={UserHome} />
-  //             <Route path="/editor" component={CodeEditor} />
-  //           </Switch>
-  //       }
-  //       {/* Displays our Login component as a fallback */}
-  //       <Route component={Login} />
-  //     </Switch>
-  // </Router>
-
-    )
+    const { isLoggedIn, handleClick } = this.props;
+    return (
+      <div>
+        {isLoggedIn ?
+          <a href="#" onClick={handleClick}>Logout</a>
+          :
+          null
+        }
+        <Router history={history}>
+          <div>
+          {!isLoggedIn ?
+            <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            </div>
+            :
+            null
+          }
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+              {isLoggedIn && (
+                <Switch>
+                  {/* Routes placed here are only available after logging in */}
+                  <Route path="/editor" component={CodeEditor} />
+                  <Route path="/home" component={UserHome} />
+                  <Route path="/" component={UserHome} />
+                </Switch>
+              )}
+              <Route component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
     userId: state.user.id
-  }
-}
+  };
+};
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
-
-    loadInitialData () {
-      console.log("loadinitialdata fired")
-      dispatch(me())
-  },
-    handleClick () {
-      dispatch(logout())
+    loadInitialData() {
+      console.log("loadinitialdata fired");
+      dispatch(me());
+    },
+    handleClick() {
+      dispatch(logout());
     }
-  }
-}
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default connect(mapState, mapDispatch)(Main)
+export default connect(mapState, mapDispatch)(Main);
 
 /**
  * PROP TYPES
@@ -140,4 +103,4 @@ Main.propTypes = {
   children: PropTypes.object,
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
