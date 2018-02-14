8c02d7e6fecba0c2d2ff7fd44bc2a39b1bbd1a32
Sam Glass <samuelglass@gmail.com>
2018-02-13 21:05:12 -0500

    frontend is working but looks god awful

--- client/components/main.js
+++ client/components/main.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
+import { Switch, Route, Link} from 'react-router-dom'
+import {logout, me} from '../store'
+import CodeEditor from './CodeEditor'
+import UserHome from './user-home'
+import {Login, Signup} from './auth-form'
+import {Router} from 'react-router'
+import history from '../history'
 
 /**
  * COMPONENT
@@ -10,33 +15,95 @@ import {logout} from '../store'
  *  else common to our entire app. The 'picture' inside the frame is the space
  *  rendered out by the component's `children`.
  */
-const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+// const Main = (props) => {
+//   const {children, handleClick, isLoggedIn} = props
 
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      test: 'case'
+    }
+  }
+
+  componentDidMount () {
+    console.log("component did mount from main fired")
+    this.props.loadInitialData()
+  }
+
+  render() {
+    const {isLoggedIn, handleClick} = this.props
+    console.log("!!!", this.props)
   return (
     <div>
+    <a href="#" onClick={handleClick}>Logout</a>
 
+    <Router history={history}>
+    <div>
+    <Link to="/login">Login</Link>
+    <Link to="/signup">Sign Up</Link>
+    <Switch>
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
+      {
+        isLoggedIn &&
 
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
+          <Switch>
+            {/* Routes placed here are only available after logging in */}
+            <Route path="/editor" component={CodeEditor} />
+            <Route path="/home" component={UserHome} />
+            <Route path="/" component={UserHome} />
+          </Switch>
+      }
+      <Route component={Login} />
 
-      {children}
+    </Switch>
     </div>
-  )
+</Router>
+
+
+    </div>
+
+    //   <nav>
+    //     {
+    //       isLoggedIn
+    //         ? <div>
+    //           {/* The navbar will show these links after you log in */}
+    //           <Link to="/home">Home</Link>
+    //           <a href="#" onClick={handleClick}>Logout</a>
+    //         </div>
+    //         : <div>
+    //           {/* The navbar will show these links before you log in */}
+    //           <Link to="/login">Login</Link>
+    //           <Link to="/signup">Sign Up</Link>
+    //         </div>
+    //     }
+    //   </nav>
+    //   <hr />
+
+    //   {children}
+    // </div>
+
+  //   <Router history={history}>
+  //     <Switch>
+  //       {/* Routes placed here are available to all visitors */}
+  //       <Route path="/login" component={Login} />
+  //       <Route path="/signup" component={Signup} />
+  //       {
+  //         isLoggedIn &&
+  //           <Switch>
+  //             {/* Routes placed here are only available after logging in */}
+  //             <Route path="/home" component={UserHome} />
+  //             <Route path="/editor" component={CodeEditor} />
+  //           </Switch>
+  //       }
+  //       {/* Displays our Login component as a fallback */}
+  //       <Route component={Login} />
+  //     </Switch>
+  // </Router>
+
+    )
+  }
 }
 
 /**
@@ -44,12 +111,18 @@ const Main = (props) => {
  */
 const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    userId: state.user.id
   }
 }
 
 const mapDispatch = (dispatch) => {
   return {
+
+    loadInitialData () {
+      console.log("loadinitialdata fired")
+      dispatch(me())
+  },
     handleClick () {
       dispatch(logout())
     }
@@ -58,7 +131,7 @@ const mapDispatch = (dispatch) => {
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Main))
+export default connect(mapState, mapDispatch)(Main)
 
 /**
  * PROP TYPES
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -13,21 +13,6 @@ export const UserHome = (props) => {
   console.log("userhome")
   return (
     <div>
-    <nav>
-    {
-      isLoggedIn
-        ? <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>Logout</a>
-        </div>
-        : <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-    }
-  </nav>
       <h3>Welcome, {email}</h3>
     </div>
   )
