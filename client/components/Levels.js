835415d1703d12182a785ca1fd21e7594ab24f0b
Sam Glass <samuelglass@gmail.com>
2018-02-18 20:34:06 -0500

    added button

--- client/components/Levels.js
+++ client/components/Levels.js
@@ -11,15 +11,11 @@ const Levels = (props) => {
 
 //gets id of last (i.e. most advanced) completed problem
   const largestProblemId = userProblems.reduce((accum, currentVal) => {
-    if (currentVal.id > accum.id && currentVal.completed) {
+    if (currentVal.id > accum.id && currentVal.UserProblem.completed) {
       accum = currentVal
     }
     return accum.id
   })
-
-
-  //when mapping over the allProblems, have the conditional on whether or not its clickable be if the problem's id is less than or equal to the largestProblemId + 1
-
   return (
 
     <div>
@@ -29,11 +25,13 @@ const Levels = (props) => {
           return (
             <div
               key={problem.id}
-              clickable={problem.id <= largestProblemId + 1 ? 'true' : 'false'}
             >
+            {/*if the problem is less than or equal to 1 + the largest completed problem, you can click it*/}
+            <button disabled={problem.id <= largestProblemId + 1 ? false : true} >
               Name: {problem.name}
               Level: {problem.level}
               Problem Number: {problem.problemNumber}
+            </button>
             </div>
           )
         })
