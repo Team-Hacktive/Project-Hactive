1a2d3f3cb0321c0f6d83c21a1e09a73b9dd5c71a
jn9cn <jannine.chan@gmail.com>
2018-02-27 17:27:57 -0500

    eagerly loading user problem

--- server/api/problems.js
+++ server/api/problems.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Problem, Dialog } = require('../db/models')
+const { Problem, Dialog, User } = require('../db/models')
 module.exports = router
 
 //get all problems
@@ -16,7 +16,7 @@ router.get('/level/:level/problem/:problemNumber', (req, res, next) => {
       level: req.params.level,
       problemNumber: req.params.problemNumber
     },
-    include: [{model: Dialog}]
+    include: [{model: Dialog}, {model: User}]
   })
   .then(problem => res.json(problem))
   .catch(next)
